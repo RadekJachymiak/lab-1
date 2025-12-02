9f30b0e34d16f47537abe6a6897701d04035e5a9
RadekJachymiak <r.jachymiak@pocztowy.pl>
2025-12-02 14:58:37 +0000

    listy i inne

--- main.tf
+++ main.tf
@@ -1,5 +1,6 @@
 variable "server-name" {
   type        = string
+  default = "server"
   description = "Na0me of a server to provision"
 }
 
@@ -12,13 +13,25 @@ locals {
 variable "number-of-servers" {
   type = number
   description = "Required number of servers"
+  default = 2
   validation {
     condition = var.number-of-servers >= local.minNumberOfServer && var.number-of-servers < local.maxNumberOfServer
     error_message = "The number of servers must be between ${local.minNumberOfServer} and ${local.maxNumberOfServer}"
   }
 }
 
+variable "number-of-disks" {
+ type = number 
+}
+
 output "out" {
   value = var.server-name
 }
 
+variable "list-of-names" {
+  type = list(string)
+}
+
+output "list-of-names" {
+  value = var.list-of-names
+}
