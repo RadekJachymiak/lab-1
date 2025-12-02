79f5758e2d3e1b038ac6a3b41311fc4aa88f6d29
RadekJachymiak <r.jachymiak@pocztowy.pl>
2025-12-02 14:47:41 +0000

    Advanced validation

--- main.tf
+++ main.tf
@@ -3,6 +3,22 @@ variable "server-name" {
   description = "Na0me of a server to provision"
 }
 
+locals {
+  minNumberOfServer = 1
+  maxNumberOfServer = 10
+}
+
+
+variable "number-of-servers" {
+  type = number
+  description = "Required number of servers"
+  validation {
+    condition = var.number-of-servers >= local.minNumberOfServer && var.number-of-servers < local.maxNumberOfServer
+    error_message = "The number of servers must be between ${local.minNumberOfServer} and ${local.maxNumberOfServer}"
+  }
+}
+
 output "out" {
   value = var.server-name
 }
+
