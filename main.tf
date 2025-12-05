748c1a3a1f7b93868da5299dad57727bd02a2900
RadekJachymiak <r.jachymiak@pocztowy.pl>
2025-12-05 13:53:50 +0000

    Z Bucketami

--- main.tf
+++ main.tf
@@ -1,50 +1,23 @@
-variable "server-name" {
-  type        = string
-  default = "server"
-  description = "Na0me of a server to provision"
-}
-
-locals {
-  minNumberOfServer = 1
-  maxNumberOfServer = 10
-}
-
-locals {
-    countOfItems = {
-        disks = 13
-        servers = 22
-        
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "6.25.0"
     }
-}
-
-
-variable "number-of-servers" {
-  type = number
-  description = "Required number of servers"
-  default = 2
-  validation {
-    condition = var.number-of-servers >= local.minNumberOfServer && var.number-of-servers < local.maxNumberOfServer
-    error_message = "The number of servers must be between ${local.minNumberOfServer} and ${local.maxNumberOfServer}"
   }
 }
 
-variable "number-of-disks" {
- type = number 
+provider "aws" {
+  # Configuration options
 }
 
-output "out" {
-  value = var.server-name
+resource "aws_s3_bucket" "my-bucket" {
+  bucket_prefix = "awsninja-3"
 }
 
-variable "list-of-names" {
-  type = list(string)
-}
-
-output "list-of-names" {
-  value = var.list-of-names
-}
-
-
-output "key-from-map" {
-  value = lookup(local.countOfItems, "disks")
+resource "aws_s3_object" "Object" {
+  for_each = fileset(path.module,"messages/*")
+  bucket = aws_s3_bucket.my-bucket.bucket
+  key = basename(each.key)
+  source = each.key
 }
