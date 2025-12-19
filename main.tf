522a12d77727a92bd660bf0273b2f9670ad6c8ef
RadekJachymiak <r.jachymiak@pocztowy.pl>
2025-12-19 11:37:07 +0000

    Serwery v2

--- main.tf
+++ main.tf
@@ -73,5 +73,11 @@ resource "aws_security_group" "allow_all" {
 }
 
 output "server_ip" {
-  value = aws_instance.app_server[*].public_ip
+  # value = aws_instance.app_server[*].public_ip
+  # value = aws_instance.app_server[*].tags("Name")
+    value = formatlist(
+        "Machin with name: %s has public ip %s",
+        aws_instance.app_server[*].tags["Name"],
+        aws_instance.app_server[*].public_ip,
+    )
 }
